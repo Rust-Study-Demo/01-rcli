823753e9c75787f0465e0da770a31eeef5a41c5a
yukine <sadmoment@foxmail.com>
2024-06-22 18:22:44 +0800

    feature: add csv cli support

---
+++ src/opts.rs
@@ -0,0 +1,36 @@
+use std::path::Path;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name="cli",version,author,about,long_about=None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or Convert CSV to JSON")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short,long,value_parser=verify_input_file)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(file: &str) -> Result<String, String> {
+    if Path::new(file).exists() {
+        Ok(file.into())
+    } else {
+        Err("File does not exist".into())
+    }
+}
